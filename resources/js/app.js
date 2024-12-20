493b01b4478228857bca59a027b2c7a38ae7adc7
Cesar Rodriguez <crcesar15@gmail.com>
2024-12-19 21:06:25 -0400

    Add BTable to the sample list component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,12 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+import "./bootstrap";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
-import './bootstrap';
-import '@fortawesome/fontawesome-free/css/all.css';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import { createApp } from "vue";
 
-import { createApp } from 'vue';
-
-/**
- * Next, we will create a fresh Vue application instance. You may then begin
- * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
- */
+import ExampleComponent from "./components/SimpleList.vue";
 
 const app = createApp({});
-
-import ExampleComponent from './components/SimpleList.vue';
-app.component('simple-list', ExampleComponent);
-app.mount('#app');
+app.component("SimpleList", ExampleComponent);
+app.mount("#app");
--- resources/js/components/SimpleList.vue
+++ resources/js/components/SimpleList.vue
@@ -1,15 +1,90 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <h1>simple list</h1>
-        </div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <BTable
+        :fields="columns"
+        :items="items"
+      >
+        <template #cell(actions)="data">
+          <BButton
+            size="sm"
+            variant="link"
+            title="edit"
+          >
+            <i class="fa fa-pencil" />
+          </BButton>
+          <BButton
+            size="sm"
+            variant="link"
+            title="delete"
+          >
+            <i class="fa fa-trash" />
+          </BButton>
+        </template>
+      </BTable>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+import { BTable, BButton } from "bootstrap-vue-next";
+
+export default {
+  components: {
+    BTable,
+    BButton,
+  },
+  data() {
+    return {
+      columns: [
+        { key: "uuid", sortable: true },
+        { key: "name", sortable: true },
+        { key: "description", sortable: true },
+        { key: "code", sortable: true },
+        { key: "status", sortable: true },
+        { key: "actions", label: "Actions" },
+      ],
+      items: [
+        {
+          uuid: "1",
+          name: "Item 1",
+          description: "Description 1",
+          code: "Code 1",
+          status: "Active",
+        },
+        {
+          uuid: "2",
+          name: "Item 2",
+          description: "Description 2",
+          code: "Code 2",
+          status: "Inactive",
+        },
+        {
+          uuid: "3",
+          name: "Item 3",
+          description: "Description 3",
+          code: "Code 3",
+          status: "Active",
+        },
+        {
+          uuid: "4",
+          name: "Item 4",
+          description: "Description 4",
+          code: "Code 4",
+          status: "Inactive",
+        },
+        {
+          uuid: "5",
+          name: "Item 5",
+          description: "Description 5",
+          code: "Code 5",
+          status: "Active",
+        },
+      ],
+    };
+  },
+  mounted() {
+    console.log("Component mounted.");
+  },
+};
 </script>
