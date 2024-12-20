814675bf119dcba2b189888754a05fe5be8f9701
Cesar Rodriguez <crcesar15@gmail.com>
2024-12-19 20:13:53 -0400

    Setup ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+     node: true,
+    browser: true,
+  },
+  globals: {
+    Vue: true,
+    _: true,
+    axios: true,
+    route: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/recommended", "airbnb-base"],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    sourceType: "module",
+    ecmaVersion: 2020,
+    babelOptions: {
+      configFile: "./babel.config.json",
+    },
+  },
+  plugins: ["vue"],
+  rules: {
+    quotes: ["error", "double"],
+    "max-len": [
+      "error",
+      {
+          code: 140,
+          ignoreComments: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["tests/**/*.js"],
+      plugins: ["jest"],
+      extends: ["plugin:jest/recommended"],
+    },
+  ],
+};
