7dbc8ed6924e815938124d071bd11cdd26e3b6de
Cesar Rodriguez <crcesar15@gmail.com>
2024-12-19 22:06:49 -0400

    Add method to the simple-list component to fetch items

--- resources/js/components/SimpleList.vue
+++ resources/js/components/SimpleList.vue
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import { BTable, BButton } from "bootstrap-vue-next";
 
 export default {
@@ -37,54 +38,29 @@ export default {
   data() {
     return {
       columns: [
-        { key: "uuid", sortable: true },
         { key: "name", sortable: true },
         { key: "description", sortable: true },
         { key: "code", sortable: true },
         { key: "status", sortable: true },
         { key: "actions", label: "Actions" },
       ],
-      items: [
-        {
-          uuid: "1",
-          name: "Item 1",
-          description: "Description 1",
-          code: "Code 1",
-          status: "Active",
-        },
-        {
-          uuid: "2",
-          name: "Item 2",
-          description: "Description 2",
-          code: "Code 2",
-          status: "Inactive",
-        },
-        {
-          uuid: "3",
-          name: "Item 3",
-          description: "Description 3",
-          code: "Code 3",
-          status: "Active",
-        },
-        {
-          uuid: "4",
-          name: "Item 4",
-          description: "Description 4",
-          code: "Code 4",
-          status: "Inactive",
-        },
-        {
-          uuid: "5",
-          name: "Item 5",
-          description: "Description 5",
-          code: "Code 5",
-          status: "Active",
-        },
-      ],
+      items: [],
     };
   },
   mounted() {
-    console.log("Component mounted.");
+    this.fetch();
+  },
+  methods: {
+    fetch() {
+      // Fetch data from API
+      axios.get(route("api.items.index"))
+        .then((response) => {
+          this.items = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
