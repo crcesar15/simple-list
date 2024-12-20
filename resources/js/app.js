a0e226a90d27827e47a853e42fbfed90da338817
Cesar Rodriguez <crcesar15@gmail.com>
2024-12-19 22:04:30 -0400

    Add ziggy to use laravel named routes in vue components

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import "./bootstrap";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
+import { ZiggyVue } from "ziggy-js";
 
 import { createApp } from "vue";
 
@@ -9,4 +10,5 @@ import ExampleComponent from "./components/SimpleList.vue";
 
 const app = createApp({});
 app.component("SimpleList", ExampleComponent);
+app.use(ZiggyVue);
 app.mount("#app");
