aae9052d2d130d7635048b3bb015d660a7c1699a
Cesar Rodriguez <crcesar15@gmail.com>
2024-12-20 00:49:40 -0400

    Fix redirection issue after registration

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect('/home');
+    }
+
     return redirect('/login');
 });
 
